2f061964ab0742d7fa16f7675b5e119157e86613
root <root@d1stkfactory>
2014-03-11 02:58:38 +0000

    Add new template for pref.xml, but only me. Fix other bugs. Add require

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,14 +5,21 @@ class puppet-pidgin ($homedir, $username, $server, $password) {
 		ensure  => "directory",
 		owner   => $homedir,
 		mode    => 700,
-		require => User[${homedir}],
 	}
  
         file { "/home/${homedir}/.purple/accounts.xml":
 		replace => "no",
 		owner   => $homedir,
 		mode    => 600,
-		require => User[${homedir}]
+		require => File["/home/$homedir/.purple"],
 		content => template('puppet-pidgin/accounts.xml'),
-       	}
+      	}
+
+        file { "/home/${homedir}/.purple/prefs.xml":
+		replace => "no",
+		owner   => $homedir,
+		mode    => 600,
+		require => File["/home/$homedir/.purple"],
+		content => template('puppet-pidgin/prefs.xml'),
+      	}
 }
